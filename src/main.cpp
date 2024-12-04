393143cf6c1af67235ab2b20d7e05821881d9606
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2024-12-04 23:14:25 +0200

    I implemented the "Pixel " class with all the functions and did some testing. I didn't use TEST.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
+#include <iostream> 
+#include "Pixel.h"
+
+using std::cout;
+using std::endl;
+
+
 int main()
 {
+	Pixel p1(219);
+	Pixel p2;
+	cout << endl << "p1 is: " << p1;
+	cout << endl << "p2 is: " << p2;
+
+	 p1 &= p2;
+	cout << endl << "p1 is: " << p1;
+	cout << endl << "p2 is: " << p2;
+
+	 
 	return 0;
 }
